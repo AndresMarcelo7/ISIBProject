06b631040d36d429e8ab9904d33430ee925b92fa
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-11-26 20:11:02 -0500

    changes

--- simple-flink-job/src/main/java/edu/eci/isiot/flinkjobs/sink/MySink.java
+++ simple-flink-job/src/main/java/edu/eci/isiot/flinkjobs/sink/MySink.java
@@ -2,30 +2,39 @@ package edu.eci.isiot.flinkjobs.sink;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.util.HashMap;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 public class MySink<IN> extends RichSinkFunction<IN> {
 
-
-
     @Override
     public void invoke(Object value, Context context) throws Exception {
-        System.out.println("kawabongaaaaaaaa");
-        System.out.println("lolazos");
-        System.out.println("kawabongax2" + value.toString());
         ObjectMapper mapper = new ObjectMapper();
         String jsonInString = mapper.writeValueAsString(value);
-        System.out.println("kawabongax3 " + jsonInString );
         OkHttpClient client = new OkHttpClient();
+        StringEntity entity = new StringEntity(jsonInString,
+                ContentType.APPLICATION_JSON);
 
-        Request request = new Request.Builder()
-                .url("https://www.google.com/")
-                .build();
-        Response response = client.newCall(request).execute();
-        System.out.println(response.body().string());
+        try {
+            HttpClient httpClient = HttpClientBuilder.create().build();
+            HttpPost request = new HttpPost("http://3.81.29.199:8082/stats");
+            request.setEntity(entity);
+
+            HttpResponse response;
+            response = httpClient.execute(request);
+            System.out.println(response.getStatusLine().getStatusCode());
+
+        } catch (IOException ex) {
+            System.out.println("aa");
+        }
     }
 }
+
